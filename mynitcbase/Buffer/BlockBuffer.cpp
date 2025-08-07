840b0ee96afb49250c305388683debcc7453ff80
shaheem <shaheem@debian.shaheem>
2025-08-07 14:39:34 +0530

    stage 2

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,65 @@
 
 #include <cstdlib>
 #include <cstring>
+
+#include "BlockBuffer.h"
+#include <cstring> // for memcpy
+
+// Constructor: initialize blockNum
+BlockBuffer::BlockBuffer(int blockNum)
+{
+    this->blockNum = blockNum;
+}
+
+// RecBuffer constructor: call BlockBuffer constructor
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer(blockNum) {}
+
+// Load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head)
+{
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read the block at this->blockNum into the buffer
+    Disk::readBlock(buffer, this->blockNum);
+
+    // Populate the fields from fixed offsets in the block
+    memcpy(&head->blockType, buffer, 4);
+    memcpy(&head->pblock, buffer + 4, 4);
+    memcpy(&head->lblock, buffer + 8, 4);
+    memcpy(&head->rblock, buffer + 12, 4);
+    memcpy(&head->numEntries, buffer + 16, 4);
+    memcpy(&head->numAttrs, buffer + 20, 4);
+    memcpy(&head->numSlots, buffer + 24, 4);
+
+    return SUCCESS;
+}
+
+// Load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec, int slotNum)
+{
+    struct HeadInfo head;
+
+    // Get the header info
+    this->getHeader(&head);
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read the block into buffer
+    Disk::readBlock(buffer, this->blockNum);
+
+    // Calculate record size and offset
+    int recordSize = attrCount * ATTR_SIZE;
+    int slotMapSize = slotCount; // 1 byte per slot
+    int offset = HEADER_SIZE + slotMapSize + (recordSize * slotNum);
+
+    // Pointer to the start of the desired record
+    unsigned char *slotPointer = buffer + offset;
+
+    // Load the record into the rec structure
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -1,19 +1,110 @@
 #include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
 #include <iostream>
+#include <vector>
 #include <string.h>
+using namespace std;
+void printAttributeCatalog()
+{
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  // creating headers for relation catalog and attribute catalog
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  // attrCatBaseSlot stores the index of current slot,
+  // which is incremented everytime an attribute entry
+  // is handled
+  for (int i = 0, attrCatSlotIndex = 0; i < relCatHeader.numEntries; i++)
+  {
+    // will store the record from the relation catalog
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int j = 0;
+    for (; j < relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal; j++, attrCatSlotIndex++)
+    {
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, attrCatSlotIndex);
+
+      // if the current attribute belongs to the current relation
+      // then we print it, which is checked by comparing names
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
+                 relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+      {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER
+                                   ? "NUM"
+                                   : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+
+      // once all the slots are traversed, we update the block number
+      // of the attrCatBuffer to the next block, and then attrCatHeader
+      // is updated to the header of next block
+      if (attrCatSlotIndex == attrCatHeader.numSlots - 1)
+      {
+        attrCatSlotIndex = -1; // implementational operation, since the loop will increment anyways
+        attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+        attrCatBuffer.getHeader(&attrCatHeader);
+      }
+    }
+
+    printf("\n");
+  }
+}
 
 int main()
 {
   Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  Disk::readBlock(buffer, 0);
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
 
-  for (int i = 0; i < BLOCK_SIZE; i++)
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  // Iterate through all relation records
+  for (int i = 0; i < relCatHeader.numEntries; i++)
   {
-    std::cout << (int)buffer[i] << " , ";
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    const char *relationName = relCatRecord[RELCAT_REL_NAME_INDEX].sVal;
+    printf("Relation: %s\n", relationName);
+
+    // Scan attribute catalog for this relation
+    for (int j = 0; j < attrCatHeader.numEntries; j++)
+    {
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      // Check if this attribute belongs to the current relation
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relationName) == 0)
+      {
+        const char *attrName = attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal;
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrName, attrType);
+      }
+    }
+
+    printf("\n");
   }
 
   return 0;
